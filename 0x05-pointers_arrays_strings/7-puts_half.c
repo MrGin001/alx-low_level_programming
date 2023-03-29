f64c6b8a1b638c56ca72458bda686e93e04a0239
MrGin001 <gsnipper1@gmail.comgit config --global user.email gsnipper1@gmail.comroot@2731A414b5b8:/alx-pre_course# git config --global user.email ganipper1@gmail.comroot@2731a4145b8:/alx-pre_course# git config --global user.email gsnipper1@gmail.com>
2023-03-29 11:24:16 -0700

    prints other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,27 @@
-#Include "main.h"
+#include "main.h"
+/**
+ * puts_half - prints half the length
+ * @str: Iteration parameter
+ * Return: 0
+ */
 
 void puts_half(char *str)
 {
 	int i;
-	int length = 0
+	int length = 0;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+		length++;
+	if (length % 2 == 0)
+	{
+		for (i = length / 2 ; str[i] != '\0' ; i++)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+	else if (length % 2 != 0)
+	{
+		for (i = (length - 1) / 2 ; str[i] != '\0' ; i++)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
 }
