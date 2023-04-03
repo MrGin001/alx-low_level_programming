e136745fd45d0172798ccf20c980b95d4e6f72e5
MrGin001 <gsnipper1@gmail.comgit config --global user.email gsnipper1@gmail.comroot@2731A414b5b8:/alx-pre_course# git config --global user.email ganipper1@gmail.comroot@2731a4145b8:/alx-pre_course# git config --global user.email gsnipper1@gmail.com>
2023-04-03 05:11:47 -0700

    5 print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int n;
+while (n < 10)
+{
+printf("%d", n);
+n += 1;
+}
+printf("\n");
+return (0);
+}
