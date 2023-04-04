4ad1d5f2ffab518d34e9e96cda2a7cb7295dbbc0
MrGin001 <gsnipper1@gmail.comgit config --global user.email gsnipper1@gmail.comroot@2731A414b5b8:/alx-pre_course# git config --global user.email ganipper1@gmail.comroot@2731a4145b8:/alx-pre_course# git config --global user.email gsnipper1@gmail.com>
2023-04-03 21:24:18 -0700

    ragged diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums -prints the result sum of two diagonals of a square matrix
+ * @a: the matrix of integers
+ * @size: matrix size
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, add1 = 0, add2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		add1 += a[i];
+		a += size;
+	}
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		add2 += a[i];
+		a += size;
+	}
+
+	printf("%d, %d\n", add1, add2);
+}
